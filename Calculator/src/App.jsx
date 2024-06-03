dd3c0f781989de6be27105c997e919e330989f94
krish858 <unknowmortal57@gmail.com>
2024-06-03 16:18:05 +0530

    created basic structure

---
+++ Calculator/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Calculator from "./Calculator"
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-full w-full">
+      <Calculator/>
+    </div>
+  )
+}
+
+export default App
