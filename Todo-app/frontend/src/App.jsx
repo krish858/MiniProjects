74094566ce1330c54ab664591bf8915ca2968cbb
krish858 <unknowmortal57@gmail.com>
2024-06-06 20:28:14 +0530

    updated the logic

--- Todo-app/frontend/src/App.jsx
+++ Todo-app/frontend/src/App.jsx
@@ -1,25 +1,30 @@
-import React, { useEffect } from "react"
-import { useState } from "react";
-import CreateTodo from "./components/CreateTodo"
-import Todoslist from "./components/Todoslist"
+import React, { useEffect, useState } from "react";
+import CreateTodo from "./components/CreateTodo";
+import Todoslist from "./components/Todoslist";
+import { RefreshContext } from "./components/context";
 
 function App() {
-  const [Todos,setTodos] = useState([]);
+  const [Todos, setTodos] = useState([]);
+  const [refresh, setRefresh] = useState(0);
 
-  useEffect(()=>{
+  useEffect(() => {
     fetch("http://localhost:3000/todos")
-    .then(async function(res){
-      const json = await res.json();
-      setTodos(json.todos);
-    });
-  },[])
- 
+      .then(async function (res) {
+        const json = await res.json();
+        setTodos(json.todos);
+      });
+  }, [refresh]);
+
+  const incrementRefresh = () => setRefresh(refresh + 1);
+
   return (
     <div className="m-2">
-      <CreateTodo></CreateTodo>
-      <Todoslist todos={Todos}/>
+      <RefreshContext.Provider value={{ refresh, incrementRefresh }}>
+        <CreateTodo />
+        <Todoslist todos={Todos} />
+      </RefreshContext.Provider>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- Todo-app/frontend/src/components/CreateTodo.jsx
+++ Todo-app/frontend/src/components/CreateTodo.jsx
@@ -1,51 +1,55 @@
-import React from 'react'
-import { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import { RefreshContext } from './context';
 
-function CreateTodo(prop) {
-  const [title,setTitle] = useState('');
-  const [description,setDescription] = useState('');
+function CreateTodo() {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const { incrementRefresh } = useContext(RefreshContext);
 
   async function registerTodo(event) {
-    event.preventDefault(); 
-    
+    event.preventDefault();
+
     const response = await fetch('http://localhost:3000/todo', {
-        method: 'POST', 
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            title,
-            description
-        }),
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        title,
+        description,
+      }),
     });
 
     const data = await response.json();
-    alert(data.msg)
+    incrementRefresh();
+    alert(data.msg);
   }
 
-  
   return (
     <div>
       <form onSubmit={registerTodo}>
-      <input 
-      type="text" 
-      placeholder='Title' 
-      className='outline-none p-2 border-black border-2'
-      value={title}
-      onChange={(e)=>setTitle(e.target.value)}
-       />
-      <br />
-      <input 
-      type="text" 
-      placeholder='Description' 
-      className='outline-none p-2 mt-2 mb-2 border-black border-2'
-      onChange={(e)=>setDescription(e.target.value)}
-      />
-      <br />
-      <button className='bg-black text-slate-400 font-semibold rounded-xl p-2 hover:text-white' type='submit'>Create Todo</button>
+        <input
+          type="text"
+          placeholder='Title'
+          className='outline-none p-2 border-black border-2'
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <br />
+        <input
+          type="text"
+          placeholder='Description'
+          className='outline-none p-2 mt-2 mb-2 border-black border-2'
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <br />
+        <button className='bg-black text-slate-400 font-semibold rounded-xl p-2 hover:text-white' type='submit'>
+          Create Todo
+        </button>
       </form>
     </div>
-  )
+  );
 }
 
-export default CreateTodo
+export default CreateTodo;
