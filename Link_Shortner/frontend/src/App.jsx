873f8a3fe108a890a86b14dfa76c18930cdd5588
krish pandey <143093900+krish858@users.noreply.github.com>
2024-08-29 19:38:41 +0530

    Update App.jsx

--- Link_Shortner/frontend/src/App.jsx
+++ Link_Shortner/frontend/src/App.jsx
@@ -1,22 +1,22 @@
-import React from 'react'
-import GenerateUrl from './component/GenerateUrl'
+import React from "react";
+import GenerateUrl from "./component/GenerateUrl";
 
 function App() {
   return (
-    <div className='flex w-full h-full'>
-    <div className='bg-black h-[100%] w-[100%]'>
-      <center>
-      <h1 className='text-6xl font-bold text-transparent bg-gradient-to-r from-blue-600 to-pink-500 bg-clip-text mt-8'>
-        Link Shortner
-      </h1>
-      <h1 className='text-slate-400'>
-        A Simple,Fast,Reliable Link Shortner 
-      </h1>
-      <GenerateUrl/>
-      </center>
+    <div className="w-full h-full bg-black">
+      <div className="h-[80%] w-[100%] flex items-center justify-center">
+        <center>
+          <h1 className="text-6xl font-bold text-transparent bg-gradient-to-r from-blue-600 to-pink-500 bg-clip-text mt-8">
+            Link Shortner
+          </h1>
+          <h1 className="text-slate-400">
+            A Simple,Fast,Reliable Link Shortner
+          </h1>
+          <GenerateUrl />
+        </center>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default App
+export default App;
